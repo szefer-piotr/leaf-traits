c983361c9d54bb7221f2d811602df61988fcfbeb
Piotr Szefer <szefer85@gmail.com>
2024-06-09 07:58:21 +0200

    Empty leaftraits pipeline.

--- src/leaf_traits/pipelines/leaftraits/nodes.py
+++ src/leaf_traits/pipelines/leaftraits/nodes.py
@@ -2,3 +2,7 @@
 This is a boilerplate pipeline 'leaftraits'
 generated using Kedro 0.19.6
 """
+
+def leaftraits():
+    print("Hello Kedro!")
+    return 1
--- src/leaf_traits/pipelines/leaftraits/pipeline.py
+++ src/leaf_traits/pipelines/leaftraits/pipeline.py
@@ -3,8 +3,16 @@ This is a boilerplate pipeline 'leaftraits'
 generated using Kedro 0.19.6
 """
 
-from kedro.pipeline import Pipeline, pipeline
+from kedro.pipeline import Pipeline, pipeline, node
+from .nodes import leaftraits
 
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([])
+    return pipeline([
+        node(
+            name="hello",
+            inputs=None,
+            outputs="not important",
+            func=leaftraits
+        )
+    ])
