74111cc996a77e78c03044a82b9986361eeaecac
Piotr Szefer <szefer85@gmail.com>
2024-06-11 10:34:45 +0200

    Add packages for the download_data_from_github node

--- src/leaf_traits/pipelines/leaftraits/nodes.py
+++ src/leaf_traits/pipelines/leaftraits/nodes.py
@@ -6,3 +6,17 @@ generated using Kedro 0.19.6
 def leaftraits():
     print("Hello Kedro!")
     return 1
+
+def download_data_from_github():
+    # Setup paths to data
+    data_path = Path("data/01_raw")
+    image_path = data_path / "train_images"
+    tabular_path = data_path / "train.csv"
+
+    if image_path.is_dir():
+        print(f"{image_path} directory exists.")
+    else:
+        print(f"Did not find {image_path} directory, downloading from GitHub...")
+        fs = fsspec.filesystem("github", org="szefer-piotr", repo="leaf_trait_data")
+        fs.get(fs.ls(""), data_path.as_posix(), recursive=False)
+    
