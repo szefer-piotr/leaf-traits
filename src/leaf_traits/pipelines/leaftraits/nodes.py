2bca7ae596231492492e1918bfe8fe16d229468a
Piotr Szefer <szefer85@gmail.com>
2024-06-11 13:48:10 +0200

    Restructure the train_data

--- src/leaf_traits/pipelines/leaftraits/nodes.py
+++ src/leaf_traits/pipelines/leaftraits/nodes.py
@@ -18,5 +18,6 @@ def download_data_from_github():
     else:
         print(f"Did not find {image_path} directory, downloading from GitHub...")
         fs = fsspec.filesystem("github", org="szefer-piotr", repo="ltdata")
-        fs.get(fs.ls(""), data_path.as_posix(), recursive=True)
+        fs.get(fs.ls("train_data"), image_path.as_posix(), recursive=True)
+        fs.get("train.csv", data_path.as_posix())
     return 1
