af0a29c310a754b391145eec26ba193e322ddb98
Piotr Szefer <szefer85@gmail.com>
2024-06-12 12:41:24 +0200

    Add test_validate_split node

--- src/leaf_traits/pipelines/leaftraits/nodes.py
+++ src/leaf_traits/pipelines/leaftraits/nodes.py
@@ -4,20 +4,22 @@ generated using Kedro 0.19.6
 """
 
 import fsspec
+import pandas as pd
 from pathlib import Path
+from sklearn.model_selection import train_test_split
 
-def leaftraits():
-    print("Hello Kedro!")
-    return 1
+
 
 def download_data_from_github():
-    
+    '''
+    Downloads the train tabular data and images from github repository.
+    '''
     data_path = Path("data/01_raw")
     image_path = data_path / "train_images"
     train_path = data_path / "train.csv"
     
     if image_path.is_dir():
-        print(f"{image_path} directory exists.")
+        print(f"On path: {image_path} directory exists.")
     
     else:
         print(f"Did not find {image_path} directory, downloading from GitHub...")
@@ -25,4 +27,25 @@ def download_data_from_github():
         fs.get(fs.ls("train_images"), image_path.as_posix(), recursive=True)
         fs.get("train.csv", train_path.as_posix())
     
-    return 1
+    return "Done"
+
+
+
+def train_validation_split(train_raw, val_size, random_state):
+    '''
+    Uploads tabular train data
+    '''
+    train, val = train_test_split(
+        train_raw, 
+        test_size=val_size, 
+        shuffle=True, 
+        random_state=random_state
+    )
+    train = train.reset_index(drop=True)
+    val = val.reset_index(drop=True)
+    print(f"Train shape: {train.shape}\nValidation shape: {val.shape}")
+    return train, val
+
+
+def binarize_images():
+    pass
--- src/leaf_traits/pipelines/leaftraits/pipeline.py
+++ src/leaf_traits/pipelines/leaftraits/pipeline.py
@@ -4,7 +4,10 @@ generated using Kedro 0.19.6
 """
 
 from kedro.pipeline import Pipeline, pipeline, node
-from .nodes import download_data_from_github
+from .nodes import (
+    download_data_from_github,
+    train_validation_split
+)
 
 
 def create_pipeline(**kwargs) -> Pipeline:
@@ -12,7 +15,13 @@ def create_pipeline(**kwargs) -> Pipeline:
         node(
             name="download_data",
             inputs=None,
-            outputs="Dataset",
+            outputs="message",
             func=download_data_from_github
+        ),
+        node(
+            name="train_val_split",
+            inputs=["train_raw", "params:N_VAL_SAMPLES", "params:SEED"],
+            outputs="train_val_tabular",
+            func=train_validation_split,
         )
     ])
