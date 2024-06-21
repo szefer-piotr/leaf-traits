46e16764595d67be6f43e1b41924aec40eb08dbb
Piotr Szefer <szefer85@gmail.com>
2024-06-21 10:47:28 +0200

    Firrst attempt to build something like an image folder.

--- src/leaf_traits/pipelines/leaftraits/nodes.py
+++ src/leaf_traits/pipelines/leaftraits/nodes.py
@@ -4,7 +4,9 @@ generated using Kedro 0.19.6
 """
 
 import fsspec
+import pandas as pd
 from pathlib import Path
+from typing import Dict, Any, Callable
 
 def leaftraits():
     print("Hello Kedro!")
@@ -26,3 +28,16 @@ def download_data_from_github():
         fs.get("train.csv", train_path.as_posix())
     
     return 1
+
+def list_files(
+        partitioned_file_list: Dict[str,Callable[[], Any]], limit: int = -1
+) -> pd.DataFrame:
+    results = []
+
+    for partition_key, partition_load_func in sorted(partitioned_file_list.items()):
+        file_path = partition_load_func()
+        results.append(file_path)
+    
+    df = pd.DataFrame(results)
+
+    return df if limit < 0 else df.sample(n=limit, random_state=42)
