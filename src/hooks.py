e62349ff9f1916de8b1783c1a8b8f94218ea071f
Piotr Szefer <szefer85@gmail.com>
2024-07-28 14:44:49 +0200

    Implementing logging runs in MLflow.

---
+++ src/hooks.py
@@ -0,0 +1,49 @@
+from kedro.framework.hooks import hook_impl
+from kedro.framework.context import KedroContext
+from kedro.pipeline import Pipeline
+
+from typing import Any
+
+import mlflow
+from mlflow.entities import RunStatus
+
+class SimpleMLflow():
+    def __init__(self):
+        self.params = None
+    
+    @hook_impl
+    def after_context_created(
+        self,
+        context: KedroContext,
+    ) -> None:
+        self.params = context.params
+
+    @hook_impl
+    def before_pipeline_run(
+        self, run_params: dict[str, Any], pipeline: Pipeline, catalog
+    ) -> None:        
+        mlflow.set_tracking_uri("http://localhost:5000")
+        # mlflow.set_experiment("vit_concat_model")
+        mlflow.start_run()
+        mlflow.set_tag("test_pipeline", "example")
+        if self.params:
+            mlflow.log_params(self.params)
+
+    @hook_impl
+    def after_pipeline_run(
+        self,
+        run_params: dict[str, Any],
+        run_result: dict[str, Any],
+        pipeline: Pipeline,
+        catalog,
+    ) -> None:
+        mlflow.end_run()
+
+    def on_pipeline_error(
+        self,
+        error: Exception,
+        run_params: dict[str, Any],
+        pipeline: Pipeline,
+        catalog,
+    ) -> None:
+        mlflow.end_run(status=RunStatus.to_string(RunStatus.FAILED))
--- src/leaf_traits/settings.py
+++ src/leaf_traits/settings.py
@@ -2,12 +2,14 @@
 from the Kedro defaults. For further information, including these default values, see
 https://docs.kedro.org/en/stable/kedro_project_setup/settings.html."""
 
+from src.hooks import SimpleMLflow
+
 # Instantiated project hooks.
 # For example, after creating a hooks.py and defining a ProjectHooks class there, do
 # from pandas_viz.hooks import ProjectHooks
 
 # Hooks are executed in a Last-In-First-Out (LIFO) order.
-# HOOKS = (ProjectHooks(),)
+HOOKS = (SimpleMLflow(),)
 
 # Installed plugins for which to disable hook auto-registration.
 # DISABLE_HOOKS_FOR_PLUGINS = ("kedro-viz",)
